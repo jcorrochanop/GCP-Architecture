e82fa3737febdc4bb77287423ad025adf2adb3bb
José Corrochano Pardo <jcorrochano@stemdo.io>
2025-12-14 13:59:27 +0100

    añadiendo bucket para el tfstate

--- iac/main.tf
+++ iac/main.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "gcs" {
+    bucket = "proyecto-terraform-state"  
+    prefix = "terraform/state"             
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -13,4 +18,3 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
